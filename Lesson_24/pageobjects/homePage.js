6a3fede3e391f374ecaad9778558e3f478127160
Anastasiya Uvarova <anastasiyauvarova1999@gmail.com>
2024-09-17 19:59:32 +0300

    'hw_24'

---
+++ Lesson_24/pageobjects/homePage.js
@@ -0,0 +1,11 @@
+import { Base } from './base.js';
+import {Search} from "./components/search.js";
+
+class HomePage extends Base {
+    constructor(){
+        super()
+        this['Search Component'] = new Search();
+    }
+}
+
+export { HomePage };
---
+++ Lesson_24/services/parameter-types.js
@@ -0,0 +1,19 @@
+import { defineParameterType } from '@wdio/cucumber-framework';
+import { Utils } from './utils';
+import { PageFactory } from '../page-objects/pageFactory.js';
+
+
+defineParameterType({
+  regexp: /"([^"\\]*(\\.[^"\\]*)*)"/,
+  name: 'locator',
+  transformer: (string) => {
+    if (string.indexOf(' > ') !== -1) {
+      const array = string.split(' > ');
+
+      return Utils.poParser(array, PageFactory);
+    }
+
+    return string;
+  },
+  useForSnippets: false,
+});
